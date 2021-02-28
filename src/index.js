1fdb7c01fbd568064b504f117f5a5d60c32a7ebd
Anastasiya Fatykhava <fatykhava@yandex.ru>
2021-02-28 20:54:03 +0300

    refactor: change avg function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(s) {
+    if (arguments.length === 0 || s.length === 0) return 0;
+    s.sort(function (a, b) {
+        return a - b;
+    });
+    return s[0];
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(p) {
+    if (arguments.length === 0 || p.length === 0) return 0;
+    p.sort(function (a, b) {
+        return a - b;
+    });
+    return p[p.length - 1];
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(l) {
+    if (arguments.length === 0 || l.length === 0) return 0;
+    const reducer = (acc, val) => acc + val;
+    return l.reduce(reducer, 0) / l.length;
 }
